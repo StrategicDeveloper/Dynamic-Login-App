fb47eec3cffec87a7f1430a0feb2129c1e189eaa
StrategicDeveloper <strategicdevelopersnetwork@gmail.com>
2017-09-12 23:13:21 -0500

    I have added bootstrap to the index file

--- index.php
+++ index.php
@@ -3,17 +3,27 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>User Login &amp; Registration</title>
+	<title>Welcome Everyone &amp; Registration</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
-	<h1><?php echo "Hello Everyone"; ?></h1>
+	<div class="container-fluid">
+		<h1><?php echo "Hello Everyone"; ?></h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
+
+	<!-- jQuery is required -->
+	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 </html>
 
